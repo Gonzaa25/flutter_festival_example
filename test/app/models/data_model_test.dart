9bd4f904121de86b13467267544aeb8e3c5ae4e6
Gonza <gonzalo.gauto@gmail.com>
2022-02-21 16:16:35 -0300

    more tests

---
+++ test/app/models/data_model_test.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:freezed_example/app/models/data/data.dart';
+
+void main() {
+  final data = Data(title: 'title', subtitle: 'subtitle');
+  group('DataModel', () {
+    test('Equality', () {
+      expect(data, Data(title: 'title', subtitle: 'subtitle'));
+    });
+    test('copyWith', () {
+      expect(data.copyWith(), Data(title: 'title', subtitle: 'subtitle'));
+    });
+
+    test('toString', () {
+      expect(data.toString(), isA<String>());
+    });
+    test('toJson', () {
+      expect(data.toJson(), isA<Map<String, dynamic>>());
+    });
+
+    test('fromJson', () {
+      expect(Data.fromJson(data.toJson()), isA<Data>());
+    });
+  });
+}
---
+++ test/home/home_screen_test.dart
@@ -0,0 +1,56 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:freezed_example/app/models/data/data.dart';
+import 'package:freezed_example/app/services/backend_service.dart';
+import 'package:freezed_example/home/cubit/data_cubit.dart';
+import 'package:freezed_example/home/screens/home_screen.dart';
+import 'package:freezed_example/home/widgets/data_content.dart';
+import 'package:freezed_example/home/widgets/home_body.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockBackendService extends Mock implements BackendService {}
+
+class MockDio extends Mock implements Dio {}
+
+void main() {
+  late BackendService backendService;
+  late DataCubit dataCubit;
+  late Widget homeScreen;
+
+  setUp(() {
+    backendService = MockBackendService();
+    dataCubit = DataCubit(backendService);
+    homeScreen = MaterialApp(
+      home: BlocProvider(
+        create: (context) => dataCubit,
+        child: const HomePage(),
+      ),
+    );
+
+    when(
+      backendService,
+    ).calls(const Symbol('getInfo')).thenAnswer(
+          (p0) => Future.value(
+            Data(
+              title: 'title',
+              subtitle: 'subtitle',
+            ),
+          ),
+        );
+  });
+
+  testWidgets('description', (tester) async {
+    await tester.pumpWidget(homeScreen);
+    expect(find.byType(HomeBody), findsOneWidget);
+  });
+
+  testWidgets('description', (tester) async {
+    await tester.pumpWidget(homeScreen);
+    final button = find.byIcon(Icons.download);
+    await tester.tap(button);
+    await tester.pumpAndSettle();
+    expect(find.byType(DataContent), findsWidgets);
+  });
+}
