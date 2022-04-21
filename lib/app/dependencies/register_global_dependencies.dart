003fb1947f5ec2f1f1137edc76e111570f412be7
Gonza <gonzalo.gauto@gmail.com>
2022-04-21 14:57:56 -0300

    refactor

--- lib/app/dependencies/register_global_dependencies.dart
+++ lib/app/dependencies/register_global_dependencies.dart
@@ -4,15 +4,12 @@ import 'package:get_it/get_it.dart';
 
 /// [registerGlobalDependencies] Setup all needed dependencies.
 void registerGlobalDependencies() {
-  final dio = Dio(
+  final _dio = Dio(
     BaseOptions(
       connectTimeout: 10000,
       baseUrl: baseUrl,
     ),
-  );
+  )..interceptors.add(LogInterceptor());
 
-  /// Log all calls.
-  dio.interceptors.add(LogInterceptor());
-
-  GetIt.instance.registerSingleton<Dio>(dio);
+  GetIt.instance.registerSingleton<Dio>(_dio);
 }
